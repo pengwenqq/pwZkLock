2ada50ff610c75b4ccb6991f43c227579f5aa48a
pengwenqq <252420467@qq.com>
2020-07-20 08:03:27 +0800

    "zk"

---
+++ src/main/java/cn/pw/pwZkLock/PwZkLockApplication.java
@@ -0,0 +1,13 @@
+package cn.pw.pwZkLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwZkLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwZkLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/pw/pwZkLock/PwZkLockApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.pw.pwZkLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwZkLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
